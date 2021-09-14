96a663758317f02f54095004be11599e1c4cec6d
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-09-14 10:12:55 +0900

    [add] typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HospitalModule } from './hospital/hospital.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [HospitalModule],
+  imports: [
+    HospitalModule,
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "ec2-44-198-146-224.compute-1.amazonaws.com",
+      "port": 5432,
+      "username": "ylonifgfdzkjau",
+      "password": "d1c8372d2893a412c5606f959ec0eb58d24e4a8433d606520da4836d1a88b13c",
+      "database": "dc1ut6929e2gfe",
+      "extra": {
+        "ssl": {
+          "rejectUnauthorized": false
+        }
+      },
+      "entities": [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
